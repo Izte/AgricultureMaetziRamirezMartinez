01c1059cb1f82b3c21df2da87686579e28d03a98
Izte <x22103848@student.ncirl.ie>
2023-04-03 12:15:39 +0100

    v2.01
    Some comment

--- GRPC/SmartAutomatedAgricultureEnvironment/src/main/java/grpc/ca/agriculture/Service1ClimateService.java
+++ GRPC/SmartAutomatedAgricultureEnvironment/src/main/java/grpc/ca/agriculture/Service1ClimateService.java
@@ -1,7 +1,10 @@
 package grpc.ca.agriculture;
 
+import grpc.ca.agriculture.climateServiceGrpc.climateServiceImplBase;
+
 public class Service1ClimateService extends climateServiceImplBase{
 	public static void main(String[] args) {
-		
+		Service1ClimateService climateserver = new Service1ClimateService();
+		int port = 50051; //This is 
 	}
 }
